2c1af8f9e56a3f4e6368a33c541050e8a2981b82
Afonso <afonsobritto@hotmail.com>
2024-01-07 14:54:25 -0300

    transformed domain in a function

--- RF.jl
+++ RF.jl
@@ -1,47 +1,72 @@
-sigma_s::float;
-sigma_r::float;
+function transformed_domain(img::Matrix{Float64}, sigma_s::Float64, sigma_r::Float64)
 
-a = [1 2 3;
-     4 5 6;
-     7 8 9]
+    # Calculating the partial derivatives
+    # I'k(x)
+    # I'k(y)
+    dx_partial = diff(img, dims=2)
+    dy_partial = diff(img, dims=1)
 
-# Calculating the partial derivatives
-dxP = diff(a, dims=2)
-dyP = diff(a, dims=1)
+    # Creating the accumulated derivative matrixes
+    dx_normal = zeros(size(img))
+    dy_normal = zeros(size(img))
 
-# Creating the accumulated derivative matrixes
-dxI = zeros(size(a))
-dyI = zeros(size(a))
+    # Getting the size of the image matrix
+    rows = size(img, 1)
+    cols = size(img, 2)
 
-# Getting the size of the image matrix
-rows = size(a, 1)
-cols = size(a, 2)
+    # Calculating the l1-normal distance of the pixels for each color channel
+    # Σ|I'k(x)|
+    # Σ|I'k(Y)|
+    # Where k is the color channel
+    for i in 1:1
+        for j in 2:cols
+            for k in 1:rows
+                dx_normal[k, j] = dx_normal[k, j] + abs(dx_partial[k, j-1])
+            end
+        end
 
-# Calculating the l1-norm distance of the pixels
-for i in 1:1
-    for j in 2:cols
-        for k in 1:rows
-            dxI[k, j] = dxI[k, j] + abs(dxP[k, j-1])
+        for j in 2:rows
+            for k in 1:cols
+                dy_normal[j, k] = dy_normal[j, k] + abs(dy_partial[j-1, k])
+            end
         end
     end
 
-    for j in 2:rows
-        for k in 1:cols
-            dyI[j, k] = dyI[j, k] + abs(dyP[j-1, k])
+    # Calculating the scaling factor so it does not have to be calculated in every iteration
+    scaling_factor::Float64 = sigma_s / sigma_r
+
+    # Calculating the horizontal and vertical derivatives of the transformed domain
+    # (1 + Σ|I'k(x)|) dx
+    # (1 + Σ|I'k(y)|) dy
+    dx_horizontal_transf = zeros(size(img))
+    dy_vertical_transf = zeros(size(img))
+
+    for i in 1:rows
+        for j in 1:cols
+            dx_horizontal_transf[i, j] = 1 +  (scaling_factor * dx_normal[i, j])
+            dy_vertical_transf[i, j] = 1 +  (scaling_factor * dy_normal[i, j])
         end
     end
+
+    # Returning the two derivatives
+    return dx_horizontal_transf, dy_vertical_transf
+
 end
 
-# Calculating the scaling factor so it does not have to be calculated in every iteration
-scaling_factor::float = sigma_s / sigma_r
+a = rand(Float64, 3, 3)  # Example input matrix
+println("Original Matrix:")
+for row in eachrow(a)
+    println(row)
+end
 
-# Calculating the horizontal and vertical derivatives of the transformed domain
-dHx = zeros(size(a))
-dVy = zeros(size(a))
+result_dx, result_dy = transformed_domain(a, 1.0, 1.0)
 
-for i in 1:rows
-    for j in 1:cols
-        dHx[i][j] = 1 +  (scaling_factor * dxI[i][j])
-        dVy[i][j] = 1 +  (scaling_factor * dyI[i][j])
-    end
+println("\nResult for dx_horizontal_transf:")
+for row in eachrow(result_dx)
+    println(row)
+end
+
+println("\nResult for dy_vertical_transf:")
+for row in eachrow(result_dy)
+    println(row)
 end
