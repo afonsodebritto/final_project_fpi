6cb680f4c6797b987601d40325df5b1db98786aa
Afonso <afonsobritto@hotmail.com>
2024-01-09 17:26:21 -0300

    using color channels now

--- RF.jl
+++ RF.jl
@@ -1,14 +1,32 @@
-function transformed_domain(img::Matrix{Float64}, sigma_s::Float64, sigma_r::Float64)
+using TestImages
+img = testimage("mandrill")
+using Images
+display(img)
 
-    # Calculating the partial derivatives
+function transformed_domain(img::AbstractArray{<:ColorTypes.RGB, 2}, sigma_s::Float64, sigma_r::Float64)
+    # Extracting each channel
+    red_channel = red.(img)
+    green_channel = green.(img)
+    blue_channel = blue.(img)
+
+    # Calculating the partial derivatives for each channel
     # I'k(x)
     # I'k(y)
-    dx_partial = diff(img, dims=2)
-    dy_partial = diff(img, dims=1)
+    dx_partial_red = diff(red_channel, dims=2)
+    dy_partial_red = diff(red_channel, dims=1)
+    dx_partial_green = diff(green_channel, dims=2)
+    dy_partial_green = diff(green_channel, dims=1)
+    dx_partial_blue = diff(blue_channel, dims=2)
+    dy_partial_blue = diff(blue_channel, dims=1)
 
-    # Creating the accumulated derivative matrixes
-    dx_normal = zeros(size(img))
-    dy_normal = zeros(size(img))
+    # Creating the accumulated derivative matrixes for each channel
+    dx_normal_red = zeros(size(red_channel))
+    dy_normal_red = zeros(size(red_channel))
+    dx_normal_green = zeros(size(green_channel))
+    dy_normal_green = zeros(size(green_channel))
+    dx_normal_blue = zeros(size(blue_channel))
+    dy_normal_blue = zeros(size(blue_channel))
+
 
     # Getting the size of the image matrix
     rows = size(img, 1)
@@ -18,35 +36,45 @@ function transformed_domain(img::Matrix{Float64}, sigma_s::Float64, sigma_r::Flo
     # Σ|I'k(x)|
     # Σ|I'k(Y)|
     # Where k is the color channel
-    for i in 1:1
-        for j in 2:cols
-            for k in 1:rows
-                dx_normal[k, j] = dx_normal[k, j] + abs(dx_partial[k, j-1])
-            end
+    for j in 2:cols
+        for k in 1:rows
+            dx_normal_red[k, j] = dx_normal_red[k, j] + abs(dx_partial_red[k, j-1])
+            dx_normal_green[k, j] = dx_normal_green[k, j] + abs(dx_partial_green[k, j-1])
+            dx_normal_blue[k, j] = dx_normal_blue[k, j] + abs(dx_partial_blue[k, j-1])
         end
+    end
 
-        for j in 2:rows
-            for k in 1:cols
-                dy_normal[j, k] = dy_normal[j, k] + abs(dy_partial[j-1, k])
-            end
+    dx_normal_red[:,2:end] = dx_normal_red[:,2:end] .+ abs(dx_partial_red)
+    dx_normal_green[:,2:end] = dx_normal_green[:,2:end] .+ abs(dx_partial_green)
+    dx_normal_blue[:,2:end] = dx_normal_blue[:,2:end] .+ abs(dx_partial_blue)
+
+    for j in 2:rows
+        for k in 1:cols
+            dy_normal_red[j, k] = dy_normal_red[j, k] + abs(dy_partial_red[j-1, k])
+            dy_normal_green[j, k] = dy_normal_green[j, k] + abs(dy_partial_green[j-1, k])
+            dy_normal_blue[j, k] = dy_normal_blue[j, k] + abs(dy_partial_blue[j-1, k])
         end
     end
 
     # Calculating the scaling factor so it does not have to be calculated in every iteration
     scaling_factor::Float64 = sigma_s / sigma_r
 
-    # Calculating the horizontal and vertical derivatives of the transformed domain
+    # Calculating the horizontal and vertical derivatives of the transformed domain of each color channel
     # (1 + Σ|I'k(x)|) dx
     # (1 + Σ|I'k(y)|) dy
-    dx_horizontal_transf = zeros(size(img))
-    dy_vertical_transf = zeros(size(img))
+    dx_horizontal_transf_red = zeros(size(red_channel))
+    dy_vertical_transf_red = zeros(size(red_channel))
+    dx_horizontal_transf_green = zeros(size(green_channel))
+    dy_vertical_transf_green = zeros(size(green_channel))
+    dx_horizontal_transf_blue = zeros(size(blue_channel))
+    dy_vertical_transf_blue = zeros(size(blue_channel))
 
-    for i in 1:rows
-        for j in 1:cols
-            dx_horizontal_transf[i, j] = 1 +  (scaling_factor * dx_normal[i, j])
-            dy_vertical_transf[i, j] = 1 +  (scaling_factor * dy_normal[i, j])
-        end
-    end
+    dx_horizontal_transf_red = ones(size(red_channel)) .+  scaling_factor * dx_normal_red
+    dy_vertical_transf_red = ones(size(red_channel)) .+  scaling_factor * dy_normal_red
+    dx_horizontal_transf_green = ones(size(green_channel)) .+  scaling_factor * dx_normal_green
+    dy_vertical_transf_green = ones(size(green_channel)) .+  scaling_factor * dy_normal_green
+    dx_horizontal_transf_blue = ones(size(blue_channel)) .+  scaling_factor * dx_normal_blue
+    dy_vertical_transf_blue = ones(size(blue_channel)) .+  scaling_factor * dy_normal_blue
 
     # Returning the two derivatives
     return dx_horizontal_transf, dy_vertical_transf
